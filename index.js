d3f93a4136ac841087c527f8249c98ce98471bad
62che <the62che@gmail.com>
2020-08-27 20:40:58 +0900

    Guarantee the script only once added. (#2)

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+const isAdsense = headItem => {
+  return (
+    headItem[0] === "script" &&
+    headItem[1] &&
+    headItem[1].src === "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+  )
+}
+
 module.exports = (options = {}, ctx) => ({
   define() {
     if (ctx.isProd) {
       ctx.siteConfig.head = ctx.siteConfig.head || [];
-      ctx.siteConfig.head.push([
-        "script",
-        {
-          "data-ad-client": options.ad_client || "ca-pub-2245427233262012",
-          async: true,
-          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        }
-      ]);
+      if (ctx.siteConfig.head.findIndex(isAdsense) < 0) {
+        ctx.siteConfig.head.push([
+          "script",
+          {
+            "data-ad-client": options.ad_client || "ca-pub-2245427233262012",
+            async: true,
+            src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          }
+        ]);
+      }
     }
     return {};
   }
